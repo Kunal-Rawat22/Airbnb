4aa51bfab0f7a88c1a57ba54f781879ca23785d8
Kunal Rawat <Kunalrawat399@gmail.com>
2023-09-28 16:49:06 +0530

    Added Dependcies

--- Api/index.js
+++ Api/index.js
@@ -21,7 +21,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://gentle-hotteok-7287fb.netlify.app",
   })
 );
 
